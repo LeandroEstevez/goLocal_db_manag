51735f62ea53e3fe94af69dde00a4d3911563c0f
Leandro Estevez <leoest.dev@gmail.com>
2021-08-18 22:35:08 -0400

    connect function

---
+++ db_man.go
@@ -0,0 +1,32 @@
+package goLocal_db_manag
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	dbname   = "gotest"
+)
+
+func ConnectDB() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"dbname=%s sslmode=disable", host, port, user, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Conected!!")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/goLocal_db_manag
+
+go 1.16
+
+require github.com/lib/pq v1.10.2
